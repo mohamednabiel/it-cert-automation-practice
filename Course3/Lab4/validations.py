03330026d4c26477b3a4b83f66daac46600626d0
mohamed nabil <mohamednabiel717@gmail.com>
2021-01-24 22:46:44 +0000

    added

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -2,6 +2,11 @@
 
 import re
 
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
